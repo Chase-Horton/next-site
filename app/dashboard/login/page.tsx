dc76f6111f30299c52b5f7a6dc50e29fa0e5f711
chase <chasejhorton@gmail.com>
2024-09-10 10:05:25 -0500

    test build

--- app/dashboard/login/page.tsx
+++ app/dashboard/login/page.tsx
@@ -10,7 +10,6 @@ import {
 } from "@/components/ui/card";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
-import ModeToggle from "@/components/ui/mode-toggle";
 
 export default function LoginForm() {
   return (
